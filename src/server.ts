aff7a792ae229503a0e32e799ef1821d030873b3
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2023-11-08 06:26:31 +0200

    Update - dependencies and close the project

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 mongoose
-    .connect(process.env.DB_CONNECTION_STRING as string)
+    .connect(process.env.DB_CONNECTION_STRING as string, { family: 4 })
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log(`Server is up and listening on ${process.env.PORT}`);
